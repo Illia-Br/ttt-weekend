3a1fb4aa338a0fa71d855b480f98ca91a200265a
Illia Brytun <elbrytun@gmail.com>
2022-02-04 14:46:26 -0500

    add state variables and cashed element references

--- js/app.js
+++ js/app.js
@@ -4,11 +4,21 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let squaresArr, turn, isWinner; 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const message = document.querySelector('message');
 
+const square0 = document.querySelector('sq0');
+const square1 = document.querySelector('sq1');
+const square2 = document.querySelector('sq2');
+const square3 = document.querySelector('sq3');
+const square4 = document.querySelector('sq4');
+const square5 = document.querySelector('sq5');
+const square6 = document.querySelector('sq6');
+const square7 = document.querySelector('sq7');
+const square8 = document.querySelector('sq8');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
